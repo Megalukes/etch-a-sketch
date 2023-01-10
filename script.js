104be223849d0cfb3fe0322e25d56a4dfb6c1091
Megalukes <megalukes@gmail.com>
2023-01-10 00:21:25 -0300

    Add buttons and finish main project

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let boxSize = 16 // 16x16
 let gridSize = 16
 
+let rainbow = false
+let erase = false
+
 function createBoxes(n = 16){
     let fullValue = n * n
     let board = document.querySelector('#board')
@@ -11,12 +14,37 @@ function createBoxes(n = 16){
         div.id = "rect" + i
 
         div.addEventListener('mouseover', (e, id = div.id) => {
-            console.log(id)
+            //console.log(id)
             let divById = document.querySelector('#' + id)
-            divById.classList.add('rectHover')
+            if (erase){
+                divById.style.backgroundColor = "white"
+                divById.classList.remove('rectHover')
+            }
+            else if (!rainbow) {
+                divById.style.backgroundColor = "black"
+                divById.classList.add('rectHover')
+            } 
+            else {
+                var st = "rgb(" + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ")"
+                divById.style.backgroundColor = st
+                divById.classList.add('rectHover')
+            }
+            
         })
         board.appendChild(div);
     }
+}
+
+
+eraser = () => {erase = true}
+
+blackColor = () => {
+    rainbow = false
+    erase = false
+}
+rainbowColor = () => {
+    rainbow = true
+    erase = false
 }
 
 createBoxes(gridSize)
